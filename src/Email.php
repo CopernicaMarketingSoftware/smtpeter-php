bb9eeb10845f65127c72d006896fcf955ab00d3e
pawel-kuznik <pawel.kuznik@copernica.com>
2017-09-05 10:33:20 +0200

    Fixed JSON generation.

--- src/Email.php
+++ src/Email.php
@@ -9,7 +9,7 @@ class Email
      */
     const RETURN_FULL = 'FULL';
     const RETURN_HEADERS = 'HDRS';
-    
+
     /**
      *  Constants for ::setNotifications() method.
      */
@@ -17,21 +17,21 @@ class Email
     const NOTIFY_FAILURE = 'FAILURE';
     const NOTIFY_SUCCESS = 'SUCCESS';
     const NOTIFY_DELAY = 'DELAY';
-    
+
     /**
      *  The REST API token
      *
      *  @var    string
      */
     private $apiToken;
-    
+
     /**
      *  The data object that will be send to SMTPeter REST API
      *
      *  @var    stdClass
      */
     private $data;
-    
+
     /**
      *  Constructor
      *
@@ -44,7 +44,7 @@ class Email
         $this->apiToken = $apiToken;
         $this->data = new \stdClass;
     }
-    
+
     /**
      *  Set mail envelope.
      *
@@ -55,11 +55,11 @@ class Email
     {
         // set the envelope
         $this->data->envelope = $envelope;
-        
+
         // allow chaining
         return $this;
     }
-    
+
     /**
      *  Set mail subject
      *
@@ -70,11 +70,11 @@ class Email
     {
         // set subject
         $this->data->subject = $subject;
-        
+
         // allow chaining
         return $this;
     }
-    
+
     /**
      *  Set mail text version.
      *
@@ -85,11 +85,11 @@ class Email
     {
         // set txt version
         $this->data->text = $text;
-        
+
         // allow chaining
         return $this;
     }
-    
+
     /**
      *  Set mail html version.
      *
@@ -100,11 +100,11 @@ class Email
     {
         // set html version
         $this->data->html = $html;
-        
+
         // allow chaining
         return $this;
     }
-    
+
     /**
      *  Set FROM address
      *
@@ -115,11 +115,11 @@ class Email
     {
         // set from address
         $this->data->from = $from;
-        
+
         // allow chaining
         return $this;
     }
-    
+
     /**
      *  Set array of email addresses as recipients.
      *
@@ -129,12 +129,12 @@ class Email
     public function setRecipients(array $addresses)
     {
         // set addresses as recipients
-        $this->data->recipient = $addresses;
-        
+        $this->data->recipients = $addresses;
+
         // allow chaining
         return $this;
     }
-    
+
     /**
      *  Append email addresses to recipients list
      *
@@ -144,15 +144,15 @@ class Email
     public function appendRecipients(array $addresses)
     {
         // ensure that we have an array
-        if (!property_exists($this->data, 'recipient') || !is_array($this->data->recipient)) $this->data->recipient = array();
-        
+        if (!property_exists($this->data, 'recipients') || !is_array($this->data->recipients)) $this->data->recipients = array();
+
         // merge current array with new array
-        $this->data->recipient = array_merge($this->data->recipient, $addresses);
-        
+        $this->data->recipients = array_merge($this->data->recipients, $addresses);
+
         // allow chaining
         return $this;
     }
-    
+
     /**
      *  Set TO addresses
      *
@@ -161,13 +161,13 @@ class Email
      */
     public function setTo(array $addresses)
     {
-        // set TO addresses 
+        // set TO addresses
         $this->data->to = $addresses;
-        
+
         // allow chaining
         return $this;
     }
-    
+
     /**
      *  Set CC addresses
      *
@@ -178,11 +178,11 @@ class Email
     {
         // set the addresses
         $this->data->cc = $addresses;
-        
+
         // allow chaining
         return $this;
     }
-    
+
     /**
      *  Set original recipent.
      *
@@ -193,19 +193,19 @@ class Email
     {
         // ensure that we have dsn property
         if (!property_exists($this->data, 'dsn')) $this->data->dsn = new \stdClass;
-        
+
         // set original recipient
         $this->data->dsn->orcpt = $recipient;
-        
+
         // allow chaining
         return $this;
     }
-    
+
     /**
      *  Set notifications types. In side array should be values from NOTIFY_*
      *  constants. self::NOTIFY_NEVER is special one. It will negate any other
      *  type.
-     * 
+     *
      *  @param  array types of notifications
      *  @return SMTPeter\Email
      */
@@ -213,17 +213,17 @@ class Email
     {
         // ensure that we have dsn property
         if (!property_exists($this->data, 'dsn')) $this->data->dsn = new \stdClass;
-        
+
         // if we have a never value it should be supplied as it is, not in array
         if (in_array('NEVER', $types)) $this->notify->dsn = self::NOTIFY_NEVER;
-        
+
         // implode notification types into one comma separated string
         else $this->notify->dsn = implode(',', $types);
-        
+
         // allow chaining
         return $this;
     }
-    
+
     /**
      *  Set how much data should be returned. As for varialbe one of RETURN_*
      *  constants should be used.
@@ -235,14 +235,14 @@ class Email
     {
         // ensure that we have dsn property
         if (!property_exists($this->data, 'dsn')) $this->data->dsn = new \stdClass;
-        
+
         // set the return type
         $this->data->dsn->ret = $return;
-        
+
         // allow chaining
         return $this;
     }
-    
+
     /**
      *  Set inline css option.
      *
@@ -253,11 +253,11 @@ class Email
     {
         // set inline css option
         $this->data->inlinecss = $inline;
-        
+
         // allow chaining
         return $this;
     }
-    
+
     /**
      *  Set track clicks option.
      *
@@ -268,11 +268,11 @@ class Email
     {
         // set track clicks option
         $this->data->trackclicks = $track;
-        
+
         // allow chaining
         return $this;
     }
-    
+
     /**
      *  Set track bounces option.
      *
@@ -283,11 +283,11 @@ class Email
     {
         // set track bounces option
         $this->data->trackbounces = $track;
-        
+
         // allow chaining
         return $this;
     }
-    
+
     /**
      *  Set track opens option.
      *
@@ -298,17 +298,27 @@ class Email
     {
         // set track opens option
         $this->data->trackopens = $track;
-        
+
         // allow chaining
         return $this;
     }
-    
+
+    /**
+     *  Method to get the JSON string that would be passed to the server.
+     *
+     *  @return string
+     */
+    public function getJSON()
+    {
+        return json_encode($this->data);
+    }
+
     /**
      *  Send the email.
      *
      *  @return mixed   If sending is successful and API acknowledge the request
      *                  this method will return TRUE. If there were any problem
-     *                  with sending ot API does not acknowledge the response 
+     *                  with sending ot API does not acknowledge the response
      *                  it will return a stdClass with explanation what went wrong.
      *                  The error message will look like this:
      *                  {
@@ -319,10 +329,10 @@ class Email
     {
         // initialize curl instance
         $handle = curl_init();
-        
+
         // encode data into string
         $dataString = json_encode($this->data);
-        
+
         // set needed curl options
         curl_setopt($handle, CURLOPT_URL, "https://www.smtpeter.com/v1/send?access_token={$this->apiToken}");
         curl_setopt($handle, CURLOPT_HTTPHEADER, array (
@@ -332,19 +342,19 @@ class Email
         curl_setopt($handle, CURLOPT_POST, 1);
         curl_setopt($handle, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($handle, CURLOPT_POSTFIELDS, $dataString);
-        
+
         // send request to API
         $result = curl_exec($handle);
-        
+
         // get http code
         $httpcode = curl_getinfo($handle, CURLINFO_HTTP_CODE);
-        
+
         // close curl instance
         curl_close($handle);
-        
+
         // if we have a 200 response we can say that sending was successful
         if ($httpcode == 200) return true;
-        
+
         // return result
         return json_decode($result);
     }
